52d33469a9e1b2af61ae4449dda1460fc6e038c6
commin0813 <commin0813@gmail.com>
2020-07-18 12:26:32 +0900

    001. spring_blog_jpa_api project 생성

---
+++ spring_blog_jpa_api/src/main/java/org/commin/blog/SpringBlogJpaApiApplication.java
@@ -0,0 +1,13 @@
+package org.commin.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogJpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogJpaApiApplication.class, args);
+	}
+
+}
---
+++ spring_blog_jpa_api/src/test/java/org/commin/blog/SpringBlogJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.commin.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogJpaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
